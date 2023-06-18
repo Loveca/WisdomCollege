7c79f181d86cbc1f1c751ce2ef4bd3fac4d49aaa
lqh <2528516305@qq.com>
2023-06-18 20:49:37 +0800

    查询teachplan属性结构-单元测试通过

---
+++ xuecheng-plus-content/xuecheng-plus-content-api/src/main/java/com/xuecheng/content/api/TeachplanController.java
@@ -0,0 +1,24 @@
+package com.xuecheng.content.api;
+
+import com.xuecheng.content.model.dto.TeachplanDto;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+//课程计划管理相关接口
+@RestController
+
+public class TeachplanController {
+
+    //查询课程计划
+    @ApiOperation("查询课程计划树形结构")
+    @ApiImplicitParam(value = "courseId",name = "课程Id",required = true,dataType = "Long",paramType = "path")
+    @GetMapping("/teachplan/{courseId}/tree-nodes")
+    public List<TeachplanDto> getTreeNodes(@PathVariable Long courseId){
+        return null;
+    }
+}
